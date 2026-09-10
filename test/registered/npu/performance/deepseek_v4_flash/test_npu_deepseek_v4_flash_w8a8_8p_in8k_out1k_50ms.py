8a085876b3ac1482c4db3092dc2b9f2aa25dbbf1
pllimax <pllimax2556769@163.com>
2026-09-10 16:56:12 +0800

    Update test_npu_deepseek_v4_flash_w8a8_8p_in8k_out1k_50ms.py
    change para

--- test/registered/npu/performance/deepseek_v4_flash/test_npu_deepseek_v4_flash_w8a8_8p_in8k_out1k_50ms.py
+++ test/registered/npu/performance/deepseek_v4_flash/test_npu_deepseek_v4_flash_w8a8_8p_in8k_out1k_50ms.py
@@ -89,7 +89,7 @@ DEEPSEEK_V4_FLASH_W8A8_8P_OTHER_ARGS = [
     "--enable-dp-lm-head",
     "--kv-cache-dtype",
     "bfloat16",
-    "--cuda-graph-bs",
+    "--cuda-graph-bs-decode",
     1,
     2,
     4,
